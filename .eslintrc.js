f1b0f3aa63f1caed19e5c8dd563141f44c8ed100
hukeke <hukeke01@baidu.com>
2018-12-06 19:09:53 +0800

    prettier-test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,38 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true,
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+            impliedStrict: true
+        }
+    },
+    plugins: ['react'],
+    rules: {
+        // "prettier/prettier": "error",
+        indent: ['error', 4], // 缩进使用4个空格
+        'no-unused-vars': 'error', // 不使用未定义的变量
+        'keyword-spacing': ['error', {after: true}], // 关键字后面要加空格
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'], // 强制使用单引号
+        semi: ['error', 'always'],
+        'react/jsx-uses-react': 'error',
+        'react/jsx-uses-vars': 'error'
+    },
+    settings: {
+        react: {
+            createClass: 'createReactClass',
+            pragma: 'React',
+            version: '16.0',
+            flowVersion: '0.53'
         },
-        "ecmaVersion": 6,
-        "sourceType": "module"
-    },
-    "plugins": ["react"],
-
-    "rules": {
-        "indent": ["error", 4],
-        "linebreak-style": ["error", "unix"],
-        "quotes": ["error", "single"],
-        "semi": ["error", "always"],
-        "react/jsx-uses-react": "error",
-        "react/jsx-uses-vars": "error",
-    },
-    "settings": {
-        "react": {
-            "createClass": "createReactClass",
-            "pragma": "React",
-            "version": "16.0",
-            "flowVersion": "0.53"
-        },
-        "propWrapperFunctions": ["forbidExtraProps"]
+        propWrapperFunctions: ['forbidExtraProps']
     }
 };
--- src/component/table.js
+++ src/component/table.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 import '../style/index.css';
 
 class Table extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
-        return <div className="showRed">
-            oopsdddd;
-        </div>;
+        return <div className="showRed">oopsdddd;</div>;
     }
 }
 export default Table;
